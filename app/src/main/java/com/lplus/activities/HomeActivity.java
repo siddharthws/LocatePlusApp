13517797837e6091315813e5c66040447810e0d2
siddharthg <siddharthg.official@gmail.com>
2018-03-15 15:01:34 +0530

    [SUPPORT][PACKAGE] Add empty class and fix package structure

---
+++ app/src/main/java/com/lplus/activities/HomeActivity.java
@@ -0,0 +1,15 @@
+package com.lplus.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.lplus.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
