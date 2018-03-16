9be06b7cc1968777d06e4eb1a4fe5375c93891b8
jagan241296 <jagan241296@gmail.com>
2018-03-16 16:22:02 +0530

    [HOME SCREEN]Constructed a home screen activity --added map functionality --added navigation drawer

--- app/src/main/java/com/lplus/activities/HomeActivity.java
+++ app/src/main/java/com/lplus/activities/HomeActivity.java
@@ -1,15 +1,300 @@
 package com.lplus.activities;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
 
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.Result;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.lplus.R;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements OnMapReadyCallback,
+                                                               GoogleMap.OnMapClickListener,
+                                                                NavigationView.OnNavigationItemSelectedListener{
 
+    private GoogleMap mMap;
+    private final int REQUEST_PERMISSION = 1;
+    private static LatLng currentLocation;
+    private FusedLocationProviderClient mFusedLocationClient;
+    private GoogleApiClient googleApiClient;
+    private static boolean isGPSOn;
+    private View mapView;
+    private NavigationView navigationView;
+
+    //-----------------------OVERRIDES------------------------//
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.menu_drawer);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        toolbar.setNavigationIcon(getResources().getDrawable(R.drawable.ic_action_back));
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.exit(1);
+            }
+        });
+        navigationView = findViewById(R.id.nav_view);
+        DrawerLayout drawer =  findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapView = mapFragment.getView();
+        mapFragment.getMapAsync(this);
+
+        //check for permissions
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // request the permission
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_PERMISSION);
+            return;
+        }
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
     }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        //if permissions given display map
+        displayMap();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_PERMISSION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    displayMap();
+                } else {
+                    System.exit(1);
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+        }
+    }
+
+    @Override
+    public void onMapClick(LatLng latLng) {}
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        switch (id)
+        {
+            case R.id.favorites: {
+
+                break;
+            }
+            case R.id.help: {
+
+                break;
+            }
+            case R.id.rate_us: {
+
+                break;
+            }
+            case R.id.exit:
+            {
+                System.exit(1);
+            }
+        }
+        DrawerLayout drawer =  findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer =  findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
+    //-----------------------PRIVATE API'S-------------------------//
+    private void displayMap() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        //set UI Settings
+        setUISettings();
+
+        //set my location enabled
+        mMap.setMyLocationEnabled(true);
+        mMap.setOnMapClickListener(this);
+    }
+
+    private void setUISettings() {
+        UiSettings uiSettings = mMap.getUiSettings();
+        uiSettings.setMapToolbarEnabled(false);
+        uiSettings.setCompassEnabled(true);
+
+        // change compass position
+        if (mapView != null &&
+                mapView.findViewById(Integer.parseInt("1")) != null) {
+            // Get the view
+            View locationCompass = ((View) mapView.findViewById(Integer.parseInt("1")).getParent()).findViewById(Integer.parseInt("5"));
+            // and next place it, on bottom right (as Google Maps app)
+            RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams)
+                    locationCompass.getLayoutParams();
+            // position on right bottom
+            layoutParams.addRule(RelativeLayout.ALIGN_PARENT_TOP, RelativeLayout.TRUE);
+            layoutParams.setMargins(0, 220,30, 0);
+        }
+    }
+
+    private void getCurrentLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        {
+            return;
+        }
+        if(mFusedLocationClient !=null) {
+            mFusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    if (location != null) {
+                        currentLocation = new LatLng(location.getLatitude(), location.getLongitude());
+                    }
+                }
+            });
+        }
+    }
+
+    private boolean checkForGpsOn()
+    {
+        isGPSOn = false;
+        if (googleApiClient == null) {
+            googleApiClient = new GoogleApiClient.Builder(getApplicationContext()).addApi(LocationServices.API).build();
+            googleApiClient.connect();
+
+            LocationRequest locationRequest = LocationRequest.create();
+            locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+            locationRequest.setInterval(30 * 1000);
+            locationRequest.setFastestInterval(5 * 1000);
+            LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder().addLocationRequest(locationRequest);
+
+            // **************************
+            builder.setAlwaysShow(true); // this is the key statement
+            // **************************
+
+            PendingResult result = LocationServices.SettingsApi.checkLocationSettings(googleApiClient, builder.build());
+            result.setResultCallback(new ResultCallback()
+             {
+                 @Override
+                 public void onResult(@NonNull Result result)
+                 {
+                     final Status status = result.getStatus();
+                     switch (status.getStatusCode())
+                     {
+
+                         case LocationSettingsStatusCodes.SUCCESS:
+                             {
+                                isGPSOn = true;
+                                break;
+                             }
+
+                         case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                             try {
+                                 status.startResolutionForResult(HomeActivity.this, 1000);
+                             } catch (IntentSender.SendIntentException e){}
+                             break;
+
+                         case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                             {
+                                 isGPSOn = false;
+                                 break;
+                             }
+                         case LocationSettingsStatusCodes.CANCELED:
+                         {
+                             isGPSOn = false;
+                             break;
+                         }
+
+                     }
+                 }
+             });
+        }
+        return isGPSOn;
+    }
+
+    //=======================PUBLIC API'S=====================//
+    public void currentLocationClick(View view)
+    {
+        if(!checkForGpsOn())
+        {
+            return;
+        }
+        getCurrentLocation();
+        if(currentLocation !=null) {
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLocation));//Moves the camera to users current longitude and latitude
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 16));
+        }
+    }
+
+    public void onFilterClick(View view)
+    {
+        Toast.makeText(this, "Clicked on filter",Toast.LENGTH_SHORT).show();
+    }
+
+    public void onAddPlaceClick(View view)
+    {
+        Toast.makeText(this, "Clicked on Add place",Toast.LENGTH_SHORT).show();
+    }
+
 }
