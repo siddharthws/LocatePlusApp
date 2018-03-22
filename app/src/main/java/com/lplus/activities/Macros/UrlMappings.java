e717c3d308e7f22542d629d372f3857e18d87cc5
jagan241296 <jagan241296@gmail.com>
2018-03-22 19:15:37 +0530

    [MARKER DIALOG] Dialog is opened on clicking on Marker

--- app/src/main/java/com/lplus/activities/Macros/UrlMappings.java
+++ app/src/main/java/com/lplus/activities/Macros/UrlMappings.java
@@ -7,8 +7,9 @@ package com.lplus.activities.Macros;
 public class UrlMappings {
 
     //functionalities
-    public static final String REGISTER_APP = "http://172.16.39.50:8080/api/auth/register";
-    public static final String FILTER_FETCH = "http://172.16.39.50:8080/api/user/getFC";
-    public static final String ADD_PLACE = "http://172.16.39.50:8080/api/user/addPlace";
-    public static final String GET_MARKERS = "http://172.16.39.50:8080/api/user/getPlaces";
+    public static final String REGISTER_APP         = "http://172.16.39.50:8080/api/auth/register";
+    public static final String FILTER_FETCH         = "http://172.16.39.50:8080/api/user/getFC";
+    public static final String ADD_PLACE            = "http://172.16.39.50:8080/api/user/addPlace";
+    public static final String GET_MARKERS          = "http://172.16.39.50:8080/api/user/getPlaces";
+    public static final String ADD_REVIEWS          = "http://172.16.39.50:8080/api/user/addReviews";
 }
---
+++ app/src/main/java/com/lplus/activities/Server/MarkerReviewServerClass.java
@@ -0,0 +1,88 @@
+package com.lplus.activities.Server;
+
+import android.content.Context;
+
+import com.lplus.activities.Interfaces.MarkerReviewInterface;
+import com.lplus.activities.Macros.Keys;
+import com.lplus.activities.Macros.UrlMappings;
+import com.lplus.activities.Objects.MarkerObject;
+import com.squareup.okhttp.RequestBody;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by Sai_Kameswari on 22-03-2018.
+ */
+
+public class MarkerReviewServerClass extends BaseServerClass {
+
+    private Context context;
+    private String review;
+    private MarkerObject markerObject;
+
+    private MarkerReviewInterface listener = null;
+    public void SetListener(MarkerReviewInterface listener)
+    {
+        this.listener = listener;
+    }
+
+    public MarkerReviewServerClass(Context context, MarkerObject markerObject, String review)
+    {
+        super(context, UrlMappings.ADD_REVIEWS);
+        this.context = context;
+        this.markerObject = markerObject;
+        this.review = review;
+    }
+
+    @Override
+    public Void doInBackground (Void... params)
+    {
+        // Init Request JSON
+        JSONObject requestJson = new JSONObject();
+
+        try
+        {
+            requestJson.put(Keys.MARKER_ID, markerObject.getMarkerID());
+            requestJson.put(Keys.MARKER_REVIEW, review);
+        }
+        catch (JSONException e)
+        {
+            return null;
+        }
+
+        // Add data to request Builder
+        requestBuilder.method("POST", RequestBody.create(JSON, requestJson.toString()));
+
+        // Call Super
+        super.doInBackground(params);
+        return null;
+    }
+
+    @Override
+    public void onPostExecute (Void result)
+    {
+
+        // Register user in preferences if server returned OK
+        if (IsResponseValid()) {
+            try {
+
+                boolean sucess = responseJson.getBoolean(Keys.AP_RESPONSE);
+                if(sucess)
+                {
+                    listener.onReviewSent();
+                }
+                else
+                {
+                    listener.onReviewFailed();
+                }
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        else {
+            listener.onReviewFailed();
+        }
+    }
+}
